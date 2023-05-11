49698e7bac270a5e80f9b0468de49032c3130afa
coolboydidi <24dejack@athenian.org>
2023-05-11 12:36:03 -0700

    Day 2 - Work on Layout

--- src/ToDoApp.java
+++ src/ToDoApp.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.Dimension;
 
 
 public class ToDoApp {
@@ -8,17 +8,38 @@ public class ToDoApp {
         JFrame frame = new JFrame("To Do App");
 
         JPanel westPanel = new JPanel();
-        JPanel projectsPanel = new JPanel();
+        westPanel.setPreferredSize(new Dimension(350, 600));
+        westPanel.setLayout(new BorderLayout());
+        JPanel eastPanel = new JPanel();
+
         JLabel projectsLabel = new JLabel("Projects");
-        projectsPanel.setLayout(new FlowLayout());
-        projectsLabel.setFont(new Font("Serif", Font.BOLD, 30));
-        projectsLabel.setForeground(Color.gray);
-        projectsPanel.add(projectsLabel);
+        projectsLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        projectsLabel.setFont(new Font("Serif", Font.BOLD, 35));
+        projectsLabel.setForeground(Color.BLACK);
+        JButton addProjectButton = new JButton("Add Project");
+        westPanel.add(addProjectButton, BorderLayout.SOUTH);
+        westPanel.add(projectsLabel, BorderLayout.NORTH);
 
 
-        westPanel.add(projectsPanel);
-        westPanel.setPreferredSize(new Dimension(370, 600));
         frame.add(westPanel, BorderLayout.WEST);
+
+        JTextField enterTask = new JTextField("Add a Task ", 25 );
+        enterTask.setPreferredSize(new Dimension(25, 35));
+        eastPanel.setLayout(new BorderLayout());
+        eastPanel.setBackground(Color.WHITE);
+
+        JPanel eastSouthPanel = new JPanel();
+        eastSouthPanel.setLayout(new FlowLayout());
+
+        JButton emailButton = new JButton("Email");
+        eastSouthPanel.add(emailButton);
+        eastSouthPanel.add(enterTask);
+        eastSouthPanel.setBackground(Color.WHITE);
+        eastPanel.add(eastSouthPanel, BorderLayout.SOUTH);
+
+
+        frame.add(eastPanel);
+
 
 
         //4. Size the frame.
